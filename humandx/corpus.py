dfce14f666169c0171e3aeb5eaa95a00aebe8c49
Pavel Kucherbaev <pavel.kucherbaev@gmail.com>
2016-07-25 17:50:31 +0200

    comments are added to the code

--- humandx/corpus.py
+++ humandx/corpus.py
@@ -1,33 +1,53 @@
-from humandx.indexing import sortQueryResults, scoreQueryResult
+from humandx.indexing import getRelevance, sortResults 
 import os 
 
+# constant defining the path to the datasource file
 DATASOURCE_FILE = "humandx/short-diagnoses.txt"
-WARNING_1_FILE_ABSENT = "Data source file '%s' does not exist"
-WARNING_2_QUERY_SHORT = "Query string should be at least 2 characters long. '%s' is only %s long"
 
+# constants defining the warning messages
+WARNING_1_FILE_ABSENT = "Data source file '%s' does not exist."
+WARNING_2_QUERY_SHORT = "Query string should be at least 2 characters long. '%s' is only %s long."
+WARNING_3_CORPUS_IS_EMPTY = "The corpus of diagnoses is empty. Please see other warnings for details."
+
+# the container to work with the corpus of diagnoses
 class Corpus:
+	
+	# initiate the corpus of diagnoses
 	def __init__(self, datasource_file = DATASOURCE_FILE):
 		self.__diagnoses = []	
 		self.populate(datasource_file)
-	
+		# if after the corpus population it is still empty we warn about it
+		if len(self.__diagnoses) == 0:
+			print(WARNING_3_CORPUS_IS_EMPTY)
+
+	# populate the corpus of diagnoses from the data-source file
 	def populate(self,datasource_file):
+		# check that file exists
 		if os.path.exists(datasource_file):
 			with open(datasource_file) as f:
+				# read the content of the file
 			    lines_of_file = f.readlines()
+			    # clean the lines read from the file from unnecessary characters
 			    self.__diagnoses = [x.strip('\n') for x in lines_of_file]
 		else:
+			# warn that the file is absent
 			print(WARNING_1_FILE_ABSENT % datasource_file)
-
+	
+	# find diagnoses that match with the query and return them sorted
 	def query(self,query):
 		results = []
+		# only perform the search if the query keyword is 2+ characters long
 		if len(query)>1:
 			for diagnose in self.__diagnoses:
-				score = scoreQueryResult(query, diagnose)
-				if (score>0):
+				# check how relevant the current diagnose is against the query keyword
+				relevance = getRelevance(query, diagnose)
+				# if the current diagnose is not irrelevant - add it to the list of results
+				if (relevance>0):
 					results.append({
-						'diagnose':diagnose, 
-						'score':score})
+						"diagnose":diagnose, 
+						"relevance":relevance})
 		else:
 			print(WARNING_2_QUERY_SHORT %(query, (len(query))))
-		results_sorted = sortQueryResults(results)
+		# sort the results 
+		results_sorted = sortResults(results)
 		return results_sorted
--- humandx/indexing.py
+++ humandx/indexing.py
@@ -1,16 +1,19 @@
-def sortQueryResults(results):
+# calculate how relevant the diagnose is to the query
+def getRelevance(query, diagnose):
+	# if relevance is 0 the diagnose is completely irrelevant
+	# if relevance is 1 the diagnose is equal to the query
+	relevance = 0
+	# NB: if full query match does not happen in diagnose the relevance is 0
+	if query in diagnose:
+		# relevance shows the size of diagnose string which query string occupies
+		relevance = round(float(len(query))/len(diagnose),3)
+	return relevance
+
+# sort the query results based on the relevance and alphabetically (for diagnoses with the same relevance)
+def sortResults(results):
 	# first sort alphabetically
 	results = sorted(results, key=lambda x: x['diagnose'])
-	# second sort based to matching scores
-	results = sorted(results, key=lambda x: x['score'], reverse=True)
+	# second sort based on relevance scores
+	results = sorted(results, key=lambda x: x['relevance'], reverse=True)
 	return results
 
-def scoreQueryResult(query, diagnose):
-	# if score is 0 the diagnose is completely irrelevant
-	# if score is 1 the diagnose is equal to the query
-	score = 0
-	# NB: if full query match does not happen in diagnose the score is 0
-	if query in diagnose:
-		# score shows the size of diagnose string which query string occupies
-		score = round(float(len(query))/len(diagnose),3)
-	return score
